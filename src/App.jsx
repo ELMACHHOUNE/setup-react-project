8c58fe8ef14857fccdfbd2e805a0f86183dbf597
ELMACHHOUNE <mohamed20rida@gmail.com>
2026-08-18 16:33:05 +0100

    react project cleaned

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold underline text-red-700">
+        Hello, World!
+      </h1>
+      <h2 className="text-2xl font-semibold text-green-700">
+        This is a simple React app.
+      </h2>
+    </div>
+  );
+}
